0f8e6caef5ea6c651a816b4cbb7287a898dc89e2
Ido-Geminer <idogeminer@gmail.com>
2024-08-27 18:03:12 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+// Initialize the chatgpt api, and then we are going to prompt a user for a message, and continue the conversation until the user ends the file
+
+//importing packages
+
+import { OpenAIApi, Configuration } from "openai";
+
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+require("dotenv").config();
+
+//step 1 - initialize chatgpt api
+
+//step 2 - create context for the api (give it some personality)
+
+//step 3 - define the function to retrieve the api message based on user input
+
+//step 4 - create a run function that requests a user input
